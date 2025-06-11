9d921158a8c3ee0df4ed615a441235a7bac90f32
Akonwi Ngoh <akonwi@gmail.com>
2025-06-11 16:51:52 -0400

    use dexie in importmap

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 	base: "/maestro/",
 	plugins: [preact(), tailwindcss()],
+	build: {
+		rollupOptions: {
+			external: ["dexie"],
+		},
+	},
 });
